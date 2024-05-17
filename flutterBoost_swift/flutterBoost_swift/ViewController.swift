d023883cc8dc258153f8f9d695a210b019df48d7
zhugaopeng <zhugaopeng@thinredline.com.cn>
2024-05-17 10:44:25 +0800

    添加冲突代码 (+3 squashed commits) Squashed commits: [ec551d7] 做出选择重新 [f15c2a5] qwewqe [92edd9d] 122333

--- flutterBoost_swift/flutterBoost_swift/ViewController.swift
+++ flutterBoost_swift/flutterBoost_swift/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "首页xyzqwe"
+        self.title = "首页xyz—devtest"
     }
 
 

dca30c0875470fcd817f05c136a99bdc546a34ae
zhugaopeng <zhugaopeng@thinredline.com.cn>
2024-05-17 16:12:32 +0800

    main

--- flutterBoost_swift/flutterBoost_swift/ViewController.swift
+++ flutterBoost_swift/flutterBoost_swift/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "首页xyzMain22"
+        self.title = "首页xyzMain"
     }
 
 

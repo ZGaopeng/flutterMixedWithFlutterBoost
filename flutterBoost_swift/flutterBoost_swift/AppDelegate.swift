b76ebd9012d8e6377767c3455098a411aa025e9c
zhugaopeng <zhugaopeng@thinredline.com.cn>
2024-05-17 15:38:26 +0800

    dev

--- flutterBoost_swift/flutterBoost_swift/AppDelegate.swift
+++ flutterBoost_swift/flutterBoost_swift/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
         }
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .gray
+        window?.backgroundColor = .red
         
         UITabBar.appearance().tintColor = UIColor.orange
         UINavigationBar.appearance().tintColor = UIColor.orange
